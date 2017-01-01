a8185b953e743395ca5c16874ea8ca66d837d658
saybe <saybe@192.168.0.19>
2017-01-01 15:45:55 -0500

    SNAKE - DISPLAY CHANGED BACK
    Changed the display class to show the full game, instead of just what
    the AI sees

--- AI - JAVA/src/Snake/Display.java
+++ AI - JAVA/src/Snake/Display.java
@@ -40,52 +40,27 @@ public class Display extends JPanel {
 		
 		int[] dim = Config.getDimensions();
 
-		float[][] vision = new float[Config.getDimensions()[0]][Config.getDimensions()[1]];
-		Vector2 headPos = null;
-		for (int i = 0; i < state.length && headPos == null; i++) {
-			for (int j = 0; j < state[i].length && headPos == null; j++) {
-				if (state[i][j] == -2) {
-					headPos = new Vector2(i, j);
-				}
-			}
+		for (int i = 0; i < state.length; i++) {
+			state[i][state[i].length - 1] = state[i][state[i].length - 1];
 		}
 
-		for (int i = (int) (-dim[0] / 2); i < (int) (dim[0] / 2) + 1; i++) {
-			for (int j = (int) (-dim[1] / 2 + 1); j < (int) (dim[1] / 2) ; j++) {
-				if (i + headPos.x >= 0 && i + headPos.x < state.length && j + headPos.y >= 0
-						&& j + headPos.y < state[0].length) {
-					vision[i + dim[0] / 2][j + dim[1] / 2 - 1] = state[i + headPos.x][j + headPos.y];
-
-					if (vision[i + dim[0] / 2][j + dim[1] / 2 - 1] == 3) {
-						vision[i + dim[0] / 2][j + dim[1] / 2 - 1] = 0;
-					}
-				} else {
-					vision[i + dim[0] / 2][j + dim[1] / 2 - 1] = 0;
-				}
-			}
-		}
-
-		for (int i = 0; i < vision.length; i++) {
-			vision[i][vision[i].length - 1] = state[i][state[i].length - 1];
-		}
-
-		for (int i = 0; i < vision.length; i++) {
-			vision[i][vision[i].length - 1] = state[i][state[i].length - 1];
+		for (int i = 0; i < state.length; i++) {
+			state[i][state[i].length - 1] = state[i][state[i].length - 1];
 		}
 		
 
-		for (int i = 0; i < vision.length; i++) {
-			for (int j = 0; j < vision[i].length; j++) {
-				if (vision[i][j] == -1) {
+		for (int i = 0; i < state.length; i++) {
+			for (int j = 0; j < state[i].length; j++) {
+				if (state[i][j] == -1) {
 					g.setColor(Color.RED);
 					g.fillRect(i * s, j * s, s, s);
-				} else if (vision[i][j] == 1) {
+				} else if (state[i][j] == 1) {
 					g.setColor(Color.blue);
 					g.fillRect(i * s, j * s, s, s);
-				} else if (vision[i][j] == -2) {
+				} else if (state[i][j] == -2) {
 					g.setColor(Color.green);
 					g.fillRect(i * s, j * s, s, s);
-				}else if (vision[i][j]==3){
+				}else if (state[i][j]==3){
 					g.setColor(Color.white);
 					g.fillRect(i * s, j * s, s, s);
 				}
